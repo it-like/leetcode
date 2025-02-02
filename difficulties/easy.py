654880827e27ff7af790c0b21a071942bfe49097
Gille <93673070+it-like@users.noreply.github.com>
2025-02-02 12:23:48 +0100

    Add todays leetcode problem

---
+++ difficulties/easy.py
@@ -0,0 +1,20 @@
+'''228. Summary range'''
+#nums = [0,1,2,4,5,7]
+#print(nums[1:])
+
+
+
+'''1752. Check if Array is Sorted and Rotated'''
+def find_violation(nums):
+    violations = 0
+    left = nums[0]
+    for right in range(1,len(nums)):
+        if left > nums[right]:
+            violations += 1
+        left = nums[right]
+    if violations == 0 or (violations == 1 and nums[-1] <= nums[0]):
+        return True
+    else:
+        return False    
+#nums = [6, 10 ,6]
+#print(find_violation(nums))
--- difficulties/medium.py
+++ difficulties/medium.py
@@ -1,5 +1,5 @@
 #medium.py
-
+import numpy as np
 
 '''209. Minimum Size Subarray Sum'''
 def minSubArrayLen(target: int, nums) -> int:
@@ -13,5 +13,21 @@ def minSubArrayLen(target: int, nums) -> int:
                 current_sum -= nums[left]
                 left += 1
         return smallest if smallest != float('inf') else 0
+#print(minSubArrayLen(7, nums =[2,3,1,2,4,3]))
 
-print(minSubArrayLen(7, nums =[2,3,1,2,4,3]))
+
+
+## testing sets
+#x = {f"{x + 1}" for x in range(9)}
+#x.add(".")
+#y = {f'{x+1}' for x in range(9) if x % 2 ==0}
+#print(x)
+#print(y)
+#print(y.issubset(x))
+#
+
+matrix = [[col+1 for col in range(9)] for row in range(9)]
+print(matrix[4:6])
+
+pi =f'{np.pi}'
+print(pi)
