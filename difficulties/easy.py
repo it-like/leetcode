ac6e38e7799981c1a6a64dda8052a16110147605
Gille <93673070+it-like@users.noreply.github.com>
2025-02-08 17:39:42 +0100

    Leetcode 1800, 17, 3160, 3427

--- difficulties/easy.py
+++ difficulties/easy.py
@@ -18,3 +18,74 @@ def find_violation(nums):
         return False    
 #nums = [6, 10 ,6]
 #print(find_violation(nums))
+
+
+
+'''1800. Maximum Ascending Subarray Sum'''
+def maxAscendingSum(nums):
+    max_sum = nums[0]
+    current_sum = nums[0]
+    for i in range(1, len(nums)):
+        if nums[i-1] < nums[i]:
+            current_sum += nums[i]
+        else:
+            max_sum = max(max_sum, current_sum)
+            current_sum = nums[i]
+    return max(current_sum, max_sum)
+
+        
+#nums=[12,17,15,13,10,11,12] 
+#print(maxAscendingSum(nums))
+
+
+
+
+'''17. Letter Combinations of a Phone Number'''
+
+def letterCombinations( digits: str):
+    res = []
+    digit_to_letter = {
+                    '2' : 'abc',
+                    '3' : 'def',
+                    '4' : 'ghi',
+                    '5' : 'jkl',
+                    '6' : 'mno',
+                    '7' : 'pqrs',
+                    '8' : 'tuv',
+                    '9' : 'xyzw'
+                    }
+    
+    def itsGoingDown(i,substr):
+        if len(substr) == len(digits):
+            res.append(substr)
+            return
+        
+        for char in digit_to_letter[digits[i]]:
+            itsGoingDown(i + 1, substr + char)
+
+    if digits:
+        itsGoingDown(0,'')
+
+    return res
+#print(letterCombinations('23'))
+
+
+
+'''3160. Find the Number of Distinct Colors Among the Balls'''
+d = {'1': 'blue'}
+d['2'] = 'orange'
+for k,v in d.items():
+    print(k)
+
+
+
+
+
+'''3427. Sum of Variable Length Subarrays'''
+def subarraySum(nums) -> int:
+    res = 0
+    for i in range(len(nums)):
+        start = max(0, i - nums[i])
+        res += sum(nums[start:i+1])
+    return res
+            
