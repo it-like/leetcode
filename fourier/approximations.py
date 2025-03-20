c4908ef8162f2716d7805fc93b101d74cd1bb462
Gille <93673070+it-like@users.noreply.github.com>
2025-03-20 20:38:20 +0100

    box and gibbs

--- fourier/approximations.py
+++ fourier/approximations.py
@@ -9,10 +9,14 @@ n = len(x)
 nquart = int(np.floor(N/4))
 
 # hat fct
-f = np.zeros_like(x)
-f[nquart: 2 * nquart] =(4/N)*np.arange(1, nquart + 1)
-f[2*nquart: 3 * nquart] =np.ones(nquart) - (4/N)*np.arange(0,nquart)
+#f = np.zeros_like(x)
+#f[nquart: 2 * nquart] =(4/N)*np.arange(1, nquart + 1)
+#f[2*nquart: 3 * nquart] =np.ones(nquart) - (4/N)*np.arange(0,nquart)
 
+
+# block fct
+f = np.zeros_like(x)
+f[nquart: 3 * nquart] = 1
 
 fig, ax = plt.subplots()
 ax.plot(x,f,'_', color = 'k')
@@ -30,21 +34,21 @@ ax.set_prop_cycle(color=colors)
 A0 = np.sum(f * np.ones_like(x)) * dx
 fFS = A0/2
 
-depth = 100
+depth = 300
 A = np.zeros(depth)
 B = np.zeros(depth)
-name = 'Fourier_approximations'
+name = 'Fourier_gibbs'
 
 for k in range(depth):
     A[k] = np.sum(f *np.cos(np.pi * (k+1)* x/L)) * dx
     B[k] = np.sum(f *np.sin(np.pi * (k+1)* x/L)) * dx
     fFS = fFS  +  A[k]*np.cos(np.pi * (k+1)* x/L) + B[k] * np.sin(np.pi * (k+1)* x/L)
-    if k < 10:
+    if k == depth -1 :
         ax.plot(x,fFS, '-')
 
 
 
-path = 'fourier/images/'
-plt.savefig(path + name)
+#path = 'fourier/images/'
+#plt.savefig(path + name)
 plt.show()
     
