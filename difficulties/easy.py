56c883eb97024054c29adec471b54653319d9c43
Gille <93673070+it-like@users.noreply.github.com>
2025-04-09 21:48:32 +0200

    Todays

--- difficulties/easy.py
+++ difficulties/easy.py
@@ -442,3 +442,20 @@ def moveZeroes( nums):
             left += 1
     return nums
 
+
+
+def minOperations(nums, k):
+    nums.sort()
+    mini = nums[0]
+    if mini < k:
+        return -1
+
+    cnt = 0
+    i = 0
+    while i < len(nums):
+        if nums[i] > k:
+            cnt += 1
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+        i += 1
+    return cnt
