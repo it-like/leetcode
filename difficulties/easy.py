958c2428f0686752168d28036aaf81fce710f713
Gille <93673070+it-like@users.noreply.github.com>
2025-04-10 21:19:44 +0200

    lllleelleeee

--- difficulties/easy.py
+++ difficulties/easy.py
@@ -459,3 +459,26 @@ def minOperations(nums, k):
                 i += 1
         i += 1
     return cnt
+
+
+
+
+
+def shortestToChar(s: str, c: str):
+
+    c_index = []
+    res = []
+    min_dis = float("inf")
+
+    for i in range(len(s)):
+        if s[i] == c:
+            c_index.append(i)
+    
+    for i in range(len(s)):
+        for num in c_index:
+            curr_dis =  abs(num - i)
+            min_dis = min(curr_dis, min_dis)
+        res.append(min_dis)
+        min_dis = float("inf") 
+        
+    return res
