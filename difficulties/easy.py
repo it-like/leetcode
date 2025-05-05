c0effe0c37d0e5791ffb0e6fb6ea9d3b68e1afa7
Gille <93673070+it-like@users.noreply.github.com>
2025-05-05 21:04:44 +0200

    add lee

--- difficulties/easy.py
+++ difficulties/easy.py
@@ -751,3 +751,11 @@ def numEquivDominoPairs(dominoes) -> int:
             ret += int(val*(val-1)/2)
     return ret
         
+
+def sumOfMultiples(n: int) -> int:
+    ret = 0
+    for i in range(1,n+1):
+        if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+            ret += i
+    return ret
+
