0ac52e992e6098229c8980561bda7118bb4bdded
Gille <93673070+it-like@users.noreply.github.com>
2025-05-28 18:27:45 +0200

    l

--- difficulties/easy.py
+++ difficulties/easy.py
@@ -1007,3 +1007,14 @@ def canAliceWin(nums):
         else:
             Ad += num
     return As > Bob-As or Ad > Bob-Ad
+
+
+def numberOfSteps(num: int) -> int:
+    ret = 0
+    while num != 0:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num -= 1
+        ret += 1
+    return ret
